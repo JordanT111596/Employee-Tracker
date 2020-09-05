d4ded637c2ed871aade9d19448dcaa6ff7f546e6
Jordan Triplett <JordanT111596@gmail.com>
2020-09-04 21:27:59 -0400

    Syntax changes

--- corporationdb.sql
+++ corporationdb.sql
@@ -1,31 +1,31 @@
---If the database exists, remove it--
+-- If the database exists, remove it --
 DROP DATABASE IF EXISTS corporation_db;
 
---Creates the corporation database--
+-- Creates the corporation database --
 CREATE DATABASE corporation_db;
 
---Uses the corporation database--
+-- Uses the corporation database --
 USE corporation_db;
 
---Creates the department table--
+-- Creates the department table --
 CREATE TABLE department (
-  id INT PRIMARY KEY,
+  id INT PRIMARY KEY AUTO_INCREMENT,
   name VARCHAR(50) NOT NULL
 );
 
---Creates the role table--
+-- Creates the role table --
 CREATE TABLE role (
-id INT PRIMARY KEY,
-title VARCHAR(30) NOT NULL,
-salary DECIMAL NOT NULL,
-department_id INT NOT NULL
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INT NOT NULL
 );
 
---Creates the employee table--
+-- Creates the employee table --
 CREATE TABLE employee (
-id INT PRIMARY KEY,
-first_name VARCHAR(30) NOT NULL,
-last_name VARCHAR(30) NOT NULL,
-role_id INT NOT NULL,
-manager_id INT
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT
 );
